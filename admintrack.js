a3fb372df3844741312a16a18eb6c4d2dad39e98
Nigeru-Maker <qndelacruz03@tip.edu.ph>
2025-05-07 04:24:33 +0800

    Update admintrack.js

--- admintrack.js
+++ admintrack.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const orders = JSON.parse(localStorage.getItem("orders")) || [];
   const tbody = document.getElementById("order-table-body");
  orders.sort((a, b) => b.orderId - a.orderId); // Sort by descending order ID
-  
   orders.forEach(order => {
     const row = document.createElement("tr");
     row.innerHTML = `
